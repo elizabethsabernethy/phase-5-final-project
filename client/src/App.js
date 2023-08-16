f69b64466e716b70253b9a336be3dfee567aae50
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-08-16 15:28:52 -0400

    adds Home component with necessary info, and adds to App component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./Home";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -14,11 +15,8 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <Switch>
-          <Route path="/testing">
-            <h1>Test Route</h1>
-          </Route>
           <Route path="/">
-            <h1>Page Count: {count}</h1>
+            <Home/>
           </Route>
         </Switch>
       </div>
---
+++ client/src/Home.js
@@ -0,0 +1,34 @@
+function Home() {
+    
+    return (
+     <div>
+        <div className="header">
+           <h1>Welcome to Ruby Spa</h1> 
+        </div>
+        <div className="blurb-contianer">
+            <h2>About</h2>
+            <p>Ruby Spa was founded in 2023 to help bring clients the most luxurious retreat they could ever experience. We offer a varity of services to allow our clients the opportunity for a full day of spa relaxtion, or indivudal services if thats all you want! Each spa provider has numerous years of experience, which you can read more about in their profiles for each department. The Spa offers appointments for hair, nails, facials, and massages. You can veiw specific services for each department through their respective page on our site. Please feel free to explore, and when you're ready, book an appointment with one or many of our amazing providers!</p>
+        </div>
+        <div className="blurb-container">
+            <h2>Contact</h2>
+            <p>If you have any questions regarding our services or providers, please feel free to contact us at anytime!
+            </p>
+            <h4>Phone: 1-800-782-9772</h4>
+            <h4>Email: front_desk@ruby_spa.com</h4>
+            <h4>Location: 123 Gem St, Ruby Falls, TN 12345</h4>
+            <h4>Hours of Operation:</h4>
+            <ul>
+                <li>Monday: Closed</li>
+                <li>Tuesday: 9 AM - 5 PM</li>
+                <li>Wednesday: 9 AM - 5 PM</li>
+                <li>Thursday: 9 AM - 5 PM</li>
+                <li>Friday: 8 AM - 7 PM</li>
+                <li>Saturday: 8 AM - 7 PM</li>
+                <li>Sunday: 10 AM - 3 PM</li>
+            </ul>
+        </div>
+     </div>
+    );
+  }
+  
+  export default Home;
