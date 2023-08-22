6bc27aae183265f90c688ab048cadd7eee82d245
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-08-22 12:54:01 -0400

    adds services button to stylist component with styling

--- client/src/Stylist.js
+++ client/src/Stylist.js
@@ -4,6 +4,7 @@ function Stylist({stylist}) {
      <div className="stylist-container">
         <h3>{stylist.name}</h3>
         <h5>{stylist.job_title}</h5>
+        <button className="services-button">Services Offered</button>
      </div>
     );
   }
