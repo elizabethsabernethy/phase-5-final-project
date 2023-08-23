faf57da78d3f7da25dd2f3c9f51a34d51f3a977a
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-08-23 17:29:22 -0400

    removes full calendar api and removes imports from book apt component

--- client/src/BookAppt.js
+++ client/src/BookAppt.js
@@ -1,10 +1,6 @@
 import { useContext} from "react";
 import { UserContext } from "./context/UserContext";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import timeGridPlugin from '@fullcalendar/timegrid'
-import interaction from '@fullcalendar/interaction'
 
 function BookAppt(){
 
@@ -14,15 +10,7 @@ function BookAppt(){
   return (
       <div>
         {user.id? (
-             <FullCalendar
-             plugins={[ dayGridPlugin, timeGridPlugin, interaction ]}
-             initialView="dayGridMonth"
-             headerToolbar={{
-                left: 'prev,next today',
-                center: 'title',
-                right: 'dayGridMonth,timeGridWeek,timeGridDay'
-              }}
-           />
+            <h1>BYYYEEE</h1>
         ):(
             history.push('/login')
         )}
