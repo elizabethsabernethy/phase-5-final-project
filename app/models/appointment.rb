741e578c0214853790ce98490e747158719676b9
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-09-07 12:24:54 -0400

    adds backend logic to render error if two appointemnts are booked for the same time and formats appointment time as well

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -8,4 +8,17 @@ class Appointment < ApplicationRecord
     validates :service_id, presence: true
     validates :date, presence: true
     validates :time, presence: true
+
+    validate :date_check
+
+    def date_check
+        app = Appointment.find_by(stylist_id: self.stylist_id, date: self.date, time: self.time)
+        if app
+            errors.add(:time, message:"not available on this date with this provider")
+        end
+    end
+
+    def time_format
+        self.time.strftime("%l:%M %p").strip
+    end
 end
--- app/serializers/appointment_serializer.rb
+++ app/serializers/appointment_serializer.rb
@@ -1,5 +1,5 @@
 class AppointmentSerializer < ActiveModel::Serializer
-  attributes :id, :stylist_id, :client_id, :service_id, :date, :time
+  attributes :id, :stylist_id, :client_id, :service_id, :date, :time, :time_format
   belongs_to :stylist
   belongs_to :client
   belongs_to :service
