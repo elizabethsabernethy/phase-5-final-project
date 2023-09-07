cf947ff31f79b0a910abc8694544eeeb258b4e37
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-09-07 16:00:41 -0400

    adds form inputs and imports to preload selected appointment info

--- client/src/EditAppointment.js
+++ client/src/EditAppointment.js
@@ -1,11 +1,56 @@
 import { useContext, useState } from "react";
 import { UserContext } from "./context/UserContext";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import 'react-time-picker/dist/TimePicker.css';
+import 'react-clock/dist/Clock.css';
+import 'react-date-picker/dist/DatePicker.css';
+import 'react-calendar/dist/Calendar.css';
+import DatePicker from "react-date-picker";
+import TimePicker from "react-time-picker";
+import { ServiceContext } from "./context/ServiceContext";
+import { StylistContext } from "./context/StylistContext";
 
 function EditAppointment({appointment}){
 
     const {user} = useContext(UserContext);
     const history = useHistory();
+
+    const {services} = useContext(ServiceContext);
+    const {stylists} = useContext(StylistContext);
+
+    const defaultCategory = stylists.filter((stylist)=> stylist.id === appointment.stylist_id)[0].job_title
+
+    console.log(appointment)
+   
+    const [time, setTime] = useState(appointment.time_format)
+    const [date, setDate] = useState(appointment.date_format)
+    const [stylist, setStylist] = useState(stylists[0])
+    const [service, setService] = useState(services[0])
+    const [category, setCategory] = useState(defaultCategory)
+   
+    const filteredStylists = stylists.filter((stylist)=> stylist.job_title === category)
+   
+    function selectStylist(e){
+       const selected = stylists.filter((stylist)=> (stylist.id).toString() === (e.target.value))
+       selected.map((stylist)=> setStylist(stylist))
+    }
+   
+    function selectCategory(e){
+       setCategory(e.target.value)
+    }
+   
+    function selectService(e){
+       const selected = services.filter((service)=> (service.id).toString() === (e.target.value))
+       selected.map((service)=> setService(service))
+    }
+   
+    function selectDate(value){
+       setDate(value)
+    }
+   
+    function selectTime(value){
+       setTime(value)
+    }
   
     function handleSave(e) {
       e.preventDefault();
@@ -31,8 +76,6 @@ function EditAppointment({appointment}){
     //   });
     }
 
-    {console.log(appointment)}
-
     return(
         <div>
           {user.id? 
@@ -41,7 +84,41 @@ function EditAppointment({appointment}){
           <div>
           <h1 className="name-container">Edit Appointment</h1>
             <form onSubmit={handleSave}>
-                <button type="submit">{"Update Appointment"}</button>
+            <div>
+                <label htmlFor="category">Category</label>
+                    <select id="category" name="category" defaultValue={category} onChange={(e) => selectCategory(e)}>
+                        <option>---</option>
+                        <option value={"Hairdresser"}>Hair</option>
+                        <option value={"Nail Technician"}>Nails</option>
+                        <option value={"Aesthetician"}>Facial</option>
+                        <option value={"Massage Therapist"}>Massage</option>
+                    </select>
+                </div>
+                <div>
+                    <label htmlFor="stylist">Provider</label>
+                    <select id="stylist" name="stylist" defaultValue={appointment.stylist_id} onChange={(e) => selectStylist(e)}>
+                    <option>---</option>
+                        {filteredStylists.map((stylist)=>{
+                            return <option key={stylist.id} value={stylist.id}>{stylist.name}</option>
+                        })}
+                    </select>
+                </div>
+                <div>            
+                    <label htmlFor="service">Service</label>
+                    <select id="service" name="service" defaultValue={appointment.service_id} onChange={(e) => selectService(e)}>
+                    <option>---</option>
+                        {stylist.services.map((service)=>{
+                            return <option key={service.id} value={service.id}>{service.name}</option>
+                        })}
+                    </select>
+                </div>
+                <div>
+                    <DatePicker onChange={(value)=> selectDate(value)} value={date} required={true} minDate={new Date()}/>
+                </div>
+                <div>
+                    <TimePicker onChange={(value)=> selectTime(value)} value={time} disableClock={true} required={true} minTime="8:00" maxTime="20:00"/>
+                </div>
+                <button id="book-button" type="submit">{"Update Appointment"}</button>
             </form>
             </div> : history.push('/profile')}
             </div> :
