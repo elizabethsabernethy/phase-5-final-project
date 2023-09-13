c6b1f67c602504d9cd4a9dbff427f10588ab0af3
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-09-13 15:42:09 -0400

    removes category, provider, and service selction for editing capabilties

--- client/src/EditAppointment.js
+++ client/src/EditAppointment.js
@@ -15,34 +15,13 @@ function EditAppointment({appointment}){
     const {user, setUser} = useContext(UserContext);
     const history = useHistory();
 
-    const {services} = useContext(ServiceContext);
     const {stylists} = useContext(StylistContext);
 
     const defaultStylist = stylists.filter((stylist)=> stylist.id === appointment.stylist_id)[0]
-    const defaultService = defaultStylist.services.filter((service)=> service.id === appointment.service_id)
-    const defaultCategory = defaultStylist.job_title
+    const defaultService = defaultStylist.services.filter((service)=> service.id === appointment.service_id)[0]
    
     const [time, setTime] = useState(appointment.form_time)
     const [date, setDate] = useState(appointment.form_date)
-    const [stylist, setStylist] = useState(defaultStylist)
-    const [service, setService] = useState(defaultService)
-    const [category, setCategory] = useState(defaultCategory)
-   
-    const filteredStylists = stylists.filter((stylist)=> stylist.job_title === category)
-   
-    function selectStylist(e){
-       const selected = stylists.filter((stylist)=> (stylist.id).toString() === (e.target.value))
-       selected.map((stylist)=> setStylist(stylist))
-    }
-   
-    function selectCategory(e){
-       setCategory(e.target.value)
-    }
-   
-    function selectService(e){
-       const selected = services.filter((service)=> (service.id).toString() === (e.target.value))
-       selected.map((service)=> setService(service))
-    }
    
     function selectDate(value){
        setDate(value)
@@ -78,8 +57,6 @@ function EditAppointment({appointment}){
         },
         body: JSON.stringify({ 
             date: date,
-            service_id: service.id,
-            stylist_id: stylist.id,
             time: time
         }),
       }).then((resp) => {
@@ -100,35 +77,7 @@ function EditAppointment({appointment}){
           <div>
           <h1 className="name-container">Edit Appointment</h1>
             <form id="edit-form" onSubmit={handleSave}>
-              <p>If you would like to change your appointment type, please make a new appointment with the correct department.</p>
-            <div>
-                <label htmlFor="category">Category</label>
-                    <select disabled={true} id="category" name="category" defaultValue={category} onChange={(e) => selectCategory(e)}>
-                        <option>---</option>
-                        <option value={"Hairdresser"}>Hair</option>
-                        <option value={"Nail Technician"}>Nails</option>
-                        <option value={"Aesthetician"}>Facial</option>
-                        <option value={"Massage Therapist"}>Massage</option>
-                    </select>
-                </div>
-                <div>
-                    <label htmlFor="stylist">Provider</label>
-                    <select id="stylist" name="stylist" defaultValue={defaultStylist.id} onChange={(e) => selectStylist(e)}>
-                    <option>---</option>
-                        {filteredStylists.map((stylist)=>{
-                            return <option key={stylist.id} value={stylist.id}>{stylist.name}</option>
-                        })}
-                    </select>
-                </div>
-                <div>            
-                    <label htmlFor="service">Service</label>
-                    <select id="service" name="service" defaultValue={appointment.service_id} onChange={(e) => selectService(e)}>
-                    <option>---</option>
-                        {stylist.services.map((service)=>{
-                            return <option key={service.id} value={service.id}>{service.name}</option>
-                        })}
-                    </select>
-                </div>
+              <h4>{defaultService.name} with {defaultStylist.name}</h4>
                 <div>
                     <DatePicker onChange={(value)=> selectDate(value)} value={date} required={true}/>
                 </div>
