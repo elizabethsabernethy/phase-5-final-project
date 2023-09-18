e6c5c9ff8afbbd8b3bf745bff4e012e7e1242309
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-09-18 11:26:53 -0400

    adds minor seed data to ensure accuracy

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# Comment out seed data before 2nd push
+
+haircut = Service.create({name: "Haircut", duration: 30, price: 50, category: "Hair"})
+sarah = Stylist.create({name: "Sarah Smith", job_title: "Hairdresser"})
+manicure = Service.create({name: "Manicure", duration: 45, price: 60, category: "Nails"})
+anna = Stylist.create({name: "Anna Calloway", job_title: "Nail Technician"})
+stylist_services = Stylist_services.create([{ stylist_id: 1, service_id: 1 }, { stylist_id: 2, service_id: 2 }])
