1e86691cfaa69139dae66ee890f9eb9bb4350824
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-09-18 14:47:41 -0400

    uncomments services relations and changes production variables in database.yml to see if render with deploy correctly

--- app/models/stylist.rb
+++ app/models/stylist.rb
@@ -2,9 +2,9 @@ class Stylist < ApplicationRecord
     has_many :appointments
     has_many :clients, through: :appointments
     has_many :stylist_services
-    # has_many :services, through: :stylist_services
+    has_many :services, through: :stylist_services
 
-    # def provider_services 
-    #     self.services.pluck(:name).uniq
-    # end
+    def provider_services 
+        self.services.pluck(:name).uniq
+    end
 end
