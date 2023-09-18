175ccacbdc832b1e2c40f2e11ccd9cb3f7461961
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2023-09-18 18:45:53 -0400

    creates custom validation to prevent duplicate records

--- app/models/stylist_service.rb
+++ app/models/stylist_service.rb
@@ -1,4 +1,13 @@
 class StylistService < ApplicationRecord
     belongs_to :service
     belongs_to :stylist
+
+    validate :relation_check
+
+    def relation_check
+        relation = StylistService.find_by(stylist_id: self.stylist_id, service_id: self.service_id)
+        if relation
+            errors.add("not unique")
+        end
+    end
 end
