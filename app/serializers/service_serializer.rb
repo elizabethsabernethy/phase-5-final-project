3c235597ea5ad2458fc6e377547468e8f9173baa
Elizabeth Abernethy <elizabethsabernethy@gmail.com>
2024-01-28 19:57:59 -0500

    Adds has_many :appointments relation

--- app/serializers/service_serializer.rb
+++ app/serializers/service_serializer.rb
@@ -1,4 +1,5 @@
 class ServiceSerializer < ActiveModel::Serializer
   attributes :id, :name, :duration, :price, :category, :service_providers
+  has_many :appointments
   has_many :stylists, through: :stylist_services
 end
